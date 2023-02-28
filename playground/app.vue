554531c509651d9aaf29090c29fc827082839f71
zouhang <1102639563@qq.com>
2023-02-28 22:13:28 +0800

    chore: update deploy

--- playground/app.vue
+++ playground/app.vue
@@ -3,7 +3,8 @@ import Button from '../components/button/index.vue'
 </script>
 
 <template>
-  <div>
+  <div class="pt-4 flex flex-col max-w-375px m-auto">
+    <h3 class="mb-4 font-bold text-6">Mand mobile less</h3>
     <Button type="primary">Click</Button>
   </div>
 </template>
