fe56966f0a636195bdd34f3d7122938ea81d14fc
zouhang <1102639563@qq.com>
2023-03-01 09:54:34 +0800

    chore: update transition

--- components/transition/index.vue
+++ components/transition/index.vue
@@ -1,4 +1,5 @@
-<script>export default {
+<script>
+export default {
   name: 'md-transition',
 
   functional: true,
@@ -7,7 +8,8 @@
     return h('transition', context.data, context.children)
   },
 }
-</script>
+
+</script>
 
 <style lang="stylus">
 .md-bounce
@@ -15,108 +17,102 @@
     animation bounce-in 300ms linear
   &-leave-active
     animation zoom-out 250ms linear
-
 .md-zoom
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform scale(0.75)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-punch
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform scale(1.35)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-slide-up
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     transform translate3d(0, 100%, 0)
   &-enter-active
     transition transform 300ms cubic-bezier(0.165, 0.84, 0.44, 1)
   &-leave-active
     transition transform 250ms cubic-bezier(0.165, 0.84, 0.44, 1)
-
 .md-slide-right
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     transform translate3d(-100%, 0, 0)
   &-enter-active
     transition transform 300ms cubic-bezier(0.165, 0.84, 0.44, 1)
   &-leave-active
     transition transform 250ms cubic-bezier(0.165, 0.84, 0.44, 1)
-
 .md-slide-left
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     transform translate3d(100%, 0, 0)
   &-enter-active
     transition transform 300ms cubic-bezier(0.165, 0.84, 0.44, 1)
   &-leave-active
     transition transform 250ms cubic-bezier(0.165, 0.84, 0.44, 1)
-
 .md-slide-down
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     transform translate3d(0, -100%, 0)
   &-enter-active
     transition transform 300ms cubic-bezier(0.165, 0.84, 0.44, 1)
   &-leave-active
     transition transform 250ms cubic-bezier(0.165, 0.84, 0.44, 1)
-
 .md-fade
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
   &-enter-active
     transition opacity 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition opacity 250ms linear
-
+.md-fade-tip
+  &-enter-from, &-leave-to
+    opacity 0.01
+  &-enter-active
+    transition opacity 250ms cubic-bezier(0.215, 0.61, 0.355, 1)
+  &-leave-active
+    transition opacity 150ms linear
 .md-fade-up
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform translate3d(0, 20%, 0)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-fade-down
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform translate3d(0, -20%, 0)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-fade-right
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform translate3d(-20%, 0, 0)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-fade-left
-  &-enter, &-leave-to
+  &-enter-from, &-leave-to
     opacity 0.01
     transform translate3d(20%, 0, 0)
   &-enter-active
     transition all 300ms cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     transition all 250ms linear
-
 .md-fly
   &-enter-active
     animation fly-in 600ms
     animation-timing-function cubic-bezier(0.215, 0.61, 0.355, 1)
   &-leave-active
     animation zoom-out 250ms
-
 @keyframes fly-in
   0%
     opacity 0.5
@@ -126,7 +122,6 @@
     transform scale(1.05) translate3d(0, -50px, 0)
   100%
     transform scale(1) translate3d(0, 0, 0)
-
 @keyframes bounce-in
   0%
     transform scale(0.5)
@@ -136,7 +131,6 @@
     transform scale(0.95)
   100%
     transform scale(1)
-
 @keyframes zoom-out
   to
     opacity 0.01
--- playground/app.vue
+++ playground/app.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import Toast from '../components/toast/index'
 import Button from '../components/button/index.vue'
+import Popup from '../components/popup/index.vue'
 
 function handleClick() {
   Toast.succeed('Click')
 }
+
+const show = ref(false)
+const showHandler = () => {
+  show.value = true
+}
 </script>
 
 <template>
-  <div class="pt-4 flex flex-col max-w-375px m-auto">
+  <div class="pt-4 px-6 flex flex-col max-w-375px m-auto gap-2">
     <h3 class="mb-4 font-bold text-6">Mand mobile less</h3>
     <Button type="primary" @click="handleClick">Click</Button>
+
+    <Button type="primary" plain @click="showHandler">Show Popup</Button>
+    <Popup v-model="show" position="center">
+      <div class="bg-#fff py-4 px-8 text-base">弹窗内容</div>
+    </Popup>
   </div>
 </template>
